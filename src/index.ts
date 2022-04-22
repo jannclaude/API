7fc7fb1bc37a9186f82af37f1dddc779c3c8b23b
Juruel Keanu Lorenzo <keanulorenzo32@gmail.com>
2022-04-22 16:26:52 +0800

    pass mongodb db to router

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
 import express, { json } from 'express';
+import mongodb from 'mongodb';
 import { loadAPIRoutes } from './modules/router.js';
 
 const port = process.env.PORT ?? 3000;
 const expressClient = express().use(json());
 
+const client = new mongodb.MongoClient(process.env.MONGODB_CONNECTION_STRING!);
+
+await client.connect();
+
+const db = client.db('medcab');
+
 expressClient.listen(port, async () => {
   console.log(`Listening on port ${port}. Loading all routes...`);
 
-  const routes = await loadAPIRoutes(expressClient);
+  const routes = await loadAPIRoutes(expressClient, db);
 
   console.log(`A total of ${routes} routes were loaded.`);
 });
--- src/modules/router.ts
+++ src/modules/router.ts
@@ -1,17 +1,17 @@
 import { join, relative } from 'path';
 import { pathToFileURL } from 'url';
 import { Express, Router } from 'express';
+import { Db } from 'mongodb';
 import { getFiles } from '../utils/helpers.js';
 import { APIRoute } from '../utils/types.js';
 
-export async function loadAPIRoutes(expressClient: Express): Promise<number> {
+export async function loadAPIRoutes(expressClient: Express, db: Db): Promise<number> {
   let totalRoutes = 0;
   let router = Router();
 
   const routes_path = join(process.cwd(), 'dist/routes');
 
   for (const route_path of getFiles(routes_path)) {
-    console.log(route_path);
     const file_path = pathToFileURL(route_path).href;
     const rel_path = relative(routes_path, route_path);
     const sections = rel_path.replace(/\\/g, '/').split('/');
@@ -24,7 +24,7 @@ export async function loadAPIRoutes(expressClient: Express): Promise<number> {
       .join('/')}`;
 
     const route = (await import(file_path)).default as APIRoute;
-    router = route(endpoint, router);
+    router = route(endpoint, router, db);
 
     totalRoutes++;
     console.log(`Route Loaded: ${endpoint}`);
