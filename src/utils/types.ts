224ea50e1891e41659fb3d522efa057120f2ae82
Juruel Keanu Lorenzo <keanulorenzo32@gmail.com>
2022-04-22 16:27:22 +0800

    add schemas

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,10 +1,43 @@
 import { Router } from 'express';
-
-export interface LoginInfo {
-  username: string;
-  password: string;
-}
+import { Db, ObjectId } from 'mongodb';
 
 export interface APIRoute {
-  (endpoint: string, router: Router): Router;
+  (endpoint: string, router: Router, db: Db): Router;
 }
+
+export type Day =
+  | 'Monday'
+  | 'Tuesday'
+  | 'Wednesday'
+  | 'Thursday'
+  | 'Friday'
+  | 'Saturday'
+  | 'Sunday';
+
+export type Container = 1 | 2 | 3 | 4 | 5;
+
+export type Patient = {
+  _id?: ObjectId;
+  name: string;
+  age: number;
+};
+
+export type Medication = {
+  _id?: ObjectId;
+  patient: ObjectId;
+  medicine: ObjectId;
+};
+
+export type Schedule = {
+  _id?: ObjectId;
+  medication: ObjectId;
+  day: Day;
+  time: string;
+};
+
+export type Medicine = {
+  _id?: ObjectId;
+  name: string;
+  quantity: number;
+  container: Container;
+};
