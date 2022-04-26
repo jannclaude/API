2e812e66598849225e3aac4e23fa219af8f857d5
Juruel Keanu Lorenzo <keanulorenzo32@gmail.com>
2022-04-26 22:04:11 +0800

    add command interfaces

---
+++ src/utils/interfaces.ts
@@ -0,0 +1,20 @@
+import { ObjectId } from 'mongodb';
+import { Container } from './types.js';
+
+export interface BaseCommand {
+  id: string;
+  type: 'ring' | 'dispense';
+  time: number;
+}
+
+export interface RingCommand extends BaseCommand {
+  type: 'ring';
+}
+
+export interface DispenseCommand extends BaseCommand {
+  type: 'dispense';
+  patient: ObjectId;
+  medicine: ObjectId;
+  medication: ObjectId;
+  container: Container;
+}
