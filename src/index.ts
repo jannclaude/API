47d75655b077eceb926d3a31038e5f6f15492dc1
Juruel Keanu Lorenzo <keanulorenzo32@gmail.com>
2022-04-26 18:49:08 +0800

    load controller

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { json } from 'express';
 import mongodb from 'mongodb';
+import { loadController } from './modules/controller.js';
 import { loadAPIRoutes } from './modules/router.js';
 
 const port = process.env.PORT ?? 3000;
@@ -12,9 +13,10 @@ await client.connect();
 export const db = client.db('medcab');
 
 expressClient.listen(port, async () => {
-  console.log(`Listening on port ${port}. Loading all routes...`);
+  console.log(`Listening on port ${port}.`);
 
   const routes = await loadAPIRoutes(expressClient);
-
   console.log(`A total of ${routes} routes were loaded.`);
+
+  loadController();
 });
