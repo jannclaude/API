bc539255085bd2922d75ef59ca613559ff8b46e0
Juruel Keanu Lorenzo <keanulorenzo32@gmail.com>
2022-05-06 04:28:48 +0800

    delete medicine relations on medicine delete

--- src/routes/medicines/_medicineId/delete.ts
+++ src/routes/medicines/_medicineId/delete.ts
@@ -5,9 +5,23 @@ import { db } from '../../../modules/db.js';
 // Delete a single medicine
 export default function (endpoint: string, router: Router): Router {
   return router.delete(endpoint, async (req, res) => {
-    const result = await db
-      .collection('medicines')
-      .deleteOne({ _id: new ObjectId(req.params.medicineId) });
+    const medicineId = new ObjectId(req.params.medicineId);
+
+    const result = await db.collection('medicines').deleteOne({ _id: medicineId });
+
+    const medications = await db.collection('medications').find({ medicine: medicineId }).toArray();
+
+    await db.collection('medications').deleteMany({
+      medication: {
+        $in: medications.map(e => e._id),
+      },
+    });
+
+    await db.collection('schedules').deleteMany({
+      medication: {
+        $in: medications.map(e => e._id),
+      },
+    });
 
     res.sendStatus(result.acknowledged && result.deletedCount > 0 ? 200 : 400);
   });
